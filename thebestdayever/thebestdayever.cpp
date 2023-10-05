12a71eb7bbbc747270c384be9d3c0d887146f124
Semyon <128502655+semy90@users.noreply.github.com>
2023-10-05 17:23:36 +0500

    ПОФИКСИЛ БАГ СО ВРЕМЕНМЕЕЕМ!!

--- thebestdayever/thebestdayever.cpp
+++ thebestdayever/thebestdayever.cpp
@@ -29,26 +29,25 @@ int main() {
             }
         }
     }
-    cout << firstday << endl;
 
-    
-    
+
     //проверка на дни и добавление месяцев
     if (firstday > 31) { deltadays = 31; summouth = 1; }        //1 проверка на февраль
     if (firstday > 31 + 28) { deltadays = 31 + 28; summouth = 2; }     //2 проверка на март
-    if (firstday > 31 * 2 + 28) { deltadays = 31 * 2 + 28; summouth = 3; }     //3 проверка на апрель
-    if (firstday > 31 * 2 + 28) { deltadays = 31 * 2 + 28; summouth = 4; }     //4 проверка на май
-    if (firstday > 31 * 2 + 28 + 30) { deltadays = 31 * 2 + 28 + 30; summouth = 5; }       //5 проверка на июнь
-    if (firstday > 31 * 3 + 28 + 30) { deltadays = 31 * 3 + 28 + 30; summouth = 6; }        //6 проверка на июль
-    if (firstday > 31 * 3 + 28 + 30 * 2) { deltadays = 31 * 3 + 28 + 30 * 2; summouth = 7; }        //7 проверка на август
-    if (firstday > 31 * 4 + 28 + 30 * 2) { deltadays = 31 * 4 + 28 + 30 * 2; summouth = 8; }        //8 проверка на сентябрь
-    if (firstday > 31 * 5 + 28 + 30 * 2) { deltadays = 31 * 5 + 28 + 30 * 2; summouth = 9; }        //9 проверка на октябрь
-    if (firstday > 31 * 5 + 28 + 30 * 3) { deltadays = 31 * 5 + 28 + 30 * 3; summouth = 10; }        //10 проверка на ноябрь
-    if (firstday > 31 * 6 + 28 + 30 * 3) { deltadays = 31 * 6 + 28 + 30 * 3; summouth = 11; }        //11 проверка на декабрь
+    if (firstday > 31 + 28 + 31) { deltadays = 31 * 2 + 28; summouth = 3; }     //3 проверка на апрель
+    if (firstday > 31 * 2 + 28 + 30) { deltadays = 31 * 2 + 28 + 30; summouth = 4; }     //4 проверка на май
+    if (firstday > 31 * 3 + 28 + 30) { deltadays = 31 * 3 + 28 + 30; summouth = 5; }       //5 проверка на июнь
+    if (firstday > 31 * 3 + 28 + 30 * 2) { deltadays = 31 * 3 + 28 + 30 * 2; summouth = 6; }        //6 проверка на июль
+    if (firstday > 31 * 4 + 28 + 30 * 2) { deltadays = 31 * 4 + 28 + 30 * 2; summouth = 7; }        //7 проверка на август
+    if (firstday > 31 * 5 + 28 + 30 * 2) { deltadays = 31 * 5 + 28 + 30 * 2; summouth = 8; }        //8 проверка на сентябрь
+    if (firstday > 31 * 5 + 28 + 30 * 3) { deltadays = 31 * 5 + 28 + 30 * 3; summouth = 9; }        //9 проверка на октябрь
+    if (firstday > 31 * 6 + 28 + 30 * 3) { deltadays = 31 * 6 + 28 + 30 * 3; summouth = 10; }        //10 проверка на ноябрь
+    if (firstday > 31 * 6 + 28 + 30 * 4) { deltadays = 31 * 6 + 28 + 30 * 4; summouth = 11; }        //11 проверка на декабрь
 
     //выполняем операцию сложения и вычитания месяцев и дней
     firstday -= deltadays;
     firstmouth += summouth;
+
 
     if (firstmouth > 12) {       //проверяем если месяцев больше чем 12
         while (firstmouth > 12) {
@@ -65,7 +64,6 @@ int main() {
             }
         }
     }
-    
 
     switch (firstmouth) {        //это свич который выбирает месяц и колво дней в месяце
     case 1:
@@ -132,6 +130,3 @@ int main() {
 
     cout << firstday << " " << firstmouth << " " << firstyear;
 }
-
-
-//04.10.2023 была сделано большую часть проекта, я почти определил правильно дату, но есть ошибка в месяцах(фиксить буду завтра!!)
